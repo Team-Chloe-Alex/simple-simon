13523aaf6dc1c388c28e79a043b7945deaf7c356
Alex Endacott <endacott.alex@gmail.com>
2017-07-10 16:38:41 -0500

    add jquery

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
     function nextMove() {
     	var random = Math.floor((Math.random() * 3) + 0);
     	sequence.push(buttons[random]);
+    	console.log("in nextMove and sequence equals " + sequence);
     	execute();
     }
 
@@ -19,26 +20,49 @@ $(document).ready(function() {
     		$(element).css('opacity', .4);
     		setTimeout(function() {
     			$(element).css('opacity', 1);
-    		}, 1000);
+    		}, 800);
     	});
     	$('.button').toggleClass('active');
+    	playGame();
     }
 
     function playGame() {
     	var play = 0;
 
-    	$('.active').click(function(e){
-    		if (e.target.id == sequence[play]) {
-    			++play;
-    		}
-    		else {
-    			gameOver();
-    		}
-    	})
+    	// while (play < sequence.length) {
+
+	    	$('.active').click(function(event){
+	    		var that = this;
+	    		$('#' + $(this).attr('id')).css('opacity', .4);
+    			setTimeout(function() {
+    				$('#' + $(that).attr('id')).css('opacity', 1);
+    			}, 300); 
+    		
+	    		if ('#' + $(this).attr('id') == sequence[play]) {
+	    			++play;
+	    			console.log('Game on and sequence equals ' + sequence.length + " and play equals " + play);
+	    		}
+	    		else {
+	    			console.log($(this).attr('id'))
+	    			gameOver();
+	    		}
+
+			    if (play == sequence.length) {
+			    	console.log("play = " + play);
+			    	$('.button').toggleClass('active');
+			    	++ count;
+		    		$('#count').html(count);
+		    		nextMove();
+		    	}
+	    	});
+	 		
+	    // }
+
     }
 
     function gameOver() {
-    	
+    	console.log("Game over");
+    	return;
     }
 
     $('#start').click(function(e) {
