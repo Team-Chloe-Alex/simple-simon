35fec6bbd6375c17d1130437bc27b09462dd8cad
Alex Endacott <endacott.alex@gmail.com>
2017-07-10 15:27:00 -0500

    starting js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,50 @@
 $(document).ready(function() {
 
+	'use strict';
 
+	var count = 0;
 
+    var sequence = [];
+
+    var buttons = ['#magenta', '#violet', '#pink', '#turquiose'];
+
+    function nextMove() {
+    	var random = Math.floor((Math.random() * 3) + 0);
+    	sequence.push(buttons[random]);
+    	execute();
+    }
+
+    function execute() {
+    	sequence.forEach(function(element, index, array) {
+    		$(element).css('opacity', .4);
+    		setTimeout(function() {
+    			$(element).css('opacity', 1);
+    		}, 1000);
+    	});
+    	$('.button').toggleClass('active');
+    }
+
+    function playGame() {
+    	var play = 0;
+
+    	$('.active').click(function(e){
+    		if (e.target.id == sequence[play]) {
+    			++play;
+    		}
+    		else {
+    			gameOver();
+    		}
+    	})
+    }
+
+    function gameOver() {
+    	
+    }
+
+    $('#start').click(function(e) {
+    	++ count;
+    	$('#count').html(count);
+    	nextMove();
+    })
 
 });
