db488ea57b55e270ac21f6fa5abc83d4a6c0e2b5
Alex Endacott <endacott.alex@gmail.com>
2017-07-10 21:31:07 -0500

    fix js bugs

--- public/js/main.js
+++ public/js/main.js
@@ -8,65 +8,75 @@ $(document).ready(function() {
 
     var buttons = ['#magenta', '#violet', '#pink', '#turquiose'];
 
+    var play = 0;
+
     function nextMove() {
     	var random = Math.floor((Math.random() * 3) + 0);
     	sequence.push(buttons[random]);
     	console.log("in nextMove and sequence equals " + sequence);
-    	execute();
+    	setTimeout(function(){
+    		execute();	
+    	}, 500);
     }
 
     function execute() {
-    	sequence.forEach(function(element, index, array) {
-    		$(element).css('opacity', .4);
-    		setTimeout(function() {
-    			$(element).css('opacity', 1);
-    		}, 800);
-    	});
+    	for (var i = 0; i < sequence.length; ++i) {
+    		(function(i){
+    			setTimeout(function(){
+			    	$(sequence[i]).css('opacity', .4);
+			    	setTimeout(function() {
+			    		$(sequence[i]).css('opacity', 1);
+			    	}, 500);
+			    }, 800 * i);
+	    	}(i));
+	    }
+    	console.log('in execute and sequence equals ' + sequence);
     	$('.button').toggleClass('active');
     	playGame();
     }
 
     function playGame() {
-    	var play = 0;
+    	play = 0;
 
-    	// while (play < sequence.length) {
+    	$('.active').click(function(event){
+    		var that = this;
+    		$('#' + $(this).attr('id')).css('opacity', .4);
+			setTimeout(function() {
+				$('#' + $(that).attr('id')).css('opacity', 1);
+			}, 300); 
+		
+    		if ('#' + $(this).attr('id') == sequence[play]) {
+    			++play;
+    			console.log('adding 1 to play, play equals ' + play);
+    		}
+    		else {
+    			console.log($(this).attr('id'))
+    			gameOver();
+    		}
 
-	    	$('.active').click(function(event){
-	    		var that = this;
-	    		$('#' + $(this).attr('id')).css('opacity', .4);
-    			setTimeout(function() {
-    				$('#' + $(that).attr('id')).css('opacity', 1);
-    			}, 300); 
-    		
-	    		if ('#' + $(this).attr('id') == sequence[play]) {
-	    			++play;
-	    			console.log('Game on and sequence equals ' + sequence.length + " and play equals " + play);
-	    		}
-	    		else {
-	    			console.log($(this).attr('id'))
-	    			gameOver();
-	    		}
-
-			    if (play == sequence.length) {
-			    	console.log("play = " + play);
+    		if (play == sequence.length) {
+		    	setTimeout(function(){
+			    	console.log("play = sequence? ... play = " + play + " and sequence = " + sequence.length);
+			    	play = 0;
 			    	$('.button').toggleClass('active');
-			    	++ count;
+			    	++count;
 		    		$('#count').html(count);
-		    		nextMove();
-		    	}
-	    	});
-	 		
-	    // }
+		    	}, 500, nextMove());
+    		}
+
+    	});
 
     }
 
     function gameOver() {
     	console.log("Game over");
-    	return;
+    	$('.button').toggleClass('active');
+    	return false;
     }
 
     $('#start').click(function(e) {
-    	++ count;
+    	document.getElementById('start').disabled = true;
+    	++count;
     	$('#count').html(count);
     	nextMove();
     })
